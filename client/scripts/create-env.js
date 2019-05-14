e9ad8a9ecced5bc1e79961caf1389e9dda8f9b8d
Andy March <mail@andymarch.co.uk>
2019-05-14 16:21:18 +0100

    Create create-env.js
    Adding script for netlify

---
+++ client/scripts/create-env.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+fs.writeFileSync('../.env',
+'VUE_APP_ISSUER=${process.env.VUE_APP_ISSUER}\n'+
+'VUE_APP_CLIENT_ID=${process.env.VUE_APP_CLIENT_ID}\n'+
+'VUE_APP_REDIRECT=${process.env.VUE_APP_REDIRECT}\n'+
+'VUE_APP_SCOPE=${process.env.VUE_APP_SCOPE}\n'+
+'VUE_APP_API_BASE_URI=${process.env.VUE_APP_API_BASE_URI}\n'+
+'VUE_APP_LOGIN_IDP_ID=${process.env.VUE_APP_LOGIN_IDP_ID}\n'+
+'VUE_APP_MATCHING_IDP_ID=${process.env.VUE_APP_MATCHING_IDP_ID}\n')
