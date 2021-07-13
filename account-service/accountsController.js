e1767f8528eb06ec121f80b0a1524969bf81f3da
Andy March <mail@andymarch.co.uk>
2021-07-13 15:51:03 +0100

    Revert "Requirement for the account service removed"
    This reverts commit b9bcbae3278838e798d37059d955eed4fb245dad.

---
+++ account-service/accountsController.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const okta = require('@okta/okta-sdk-nodejs');
+
+const client = new okta.Client({
+  orgUrl: process.env.ORG_URL,
+  token: process.env.API_KEY    // Obtained from Developer Dashboard
+});
+
+exports.get_account_details = function(req,res){
+    console.log("get account called for "+req.params.accountid)
+    client.getUser(req.params.accountid)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+exports.link_facebook = function(req,res){
+    console.log("link account called for "+req.params.accountid+" to "+req.body.facebook_email)
+    client.getUser(req.params.accountid)
+    .then(user => {
+        user.profile.social_facebook_email = req.body.facebook_email;
+        user.update()
+        .then(user => {
+            console.log("Account updated")
+            res.json(user)
+        })
+    })
+    .catch(error => {
+    console.log(error)
+    res.sendStatus(500)
+    })
+}
+
+exports.unlink_facebook = function(req,res){
+    console.log("unlink account called for "+req.params.accountid)
+    client.getUser(req.params.accountid)
+    .then(user => {
+        user.profile.social_facebook_email = "";
+        user.update()
+        .then(user => res.json(user))
+    })
+    .catch(error => {
+    console.log(error)
+    res.sendStatus(500)
+})
+}
+
+exports.link_google = function(req,res){
+    console.log("link account called for "+req.params.accountid+" to "+req.body.google_email)
+    client.getUser(req.params.accountid)
+    .then(user => {
+        user.profile.social_google_email = req.body.google_email;
+        user.update()
+        .then(user => {
+            console.log("Account updated")
+            res.json(user)
+        })
+    })
+    .catch(error => {
+    console.log(error)
+    res.sendStatus(500)
+    })
+}
+
+exports.unlink_google = function(req,res){
+    console.log("unlink account called for "+req.params.accountid)
+    client.getUser(req.params.accountid)
+    .then(user => {
+        user.profile.social_google_email = "";
+        user.update()
+        .then(user => res.json(user))
+    })
+    .catch(error => {
+    console.log(error)
+    res.sendStatus(500)
+    })
+}
---
+++ account-service/routes.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = function(app){
+    var account = require('./accountsController');
+
+app.route('/account/:accountid')
+    .get(account.get_account_details)
+
+app.route('/account/:accountid/facebook')
+    .post(account.link_facebook)
+    .delete(account.unlink_facebook)
+
+app.route('/account/:accountid/google')
+    .post(account.link_google)
+    .delete(account.unlink_google)
+};
